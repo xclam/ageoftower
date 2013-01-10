fe0ae469f17cc24673784b95431e60eac0fedb17
screwt <ben_ju_79@hotmail.fr>
2013-01-11 00:13:30 +0100

    Update age-of-tower/src/com/nsx/ageoftower/screen/SplashScreen.java
    Nettoyage.

--- age-of-tower/src/com/nsx/ageoftower/screen/SplashScreen.java
+++ age-of-tower/src/com/nsx/ageoftower/screen/SplashScreen.java
@@ -15,80 +15,70 @@ import com.nsx.ageoftower.AgeOfTower;
 public class SplashScreen extends AbstractScreen{
 
 	private Texture _mSplashTexture;
-    //private TextureRegion _mSplashTextureRegion;
-    
-    public SplashScreen(AgeOfTower aot) {
-		// TODO Auto-generated constructor stub
-    	super(aot);
+
+	public SplashScreen(AgeOfTower aot) {
+		super(aot);
 	}
 
 	@Override
 	public void resize(int width, int height) {
-		// TODO Auto-generated method stub
 		super.resize(width, height);
 		
 		_mStage.clear();
 		
 		// in the image atlas, our splash image begins at (0,0) of the
-        // upper-left corner and has a dimension of 512x301
-        TextureRegion splashRegion = new TextureRegion( _mSplashTexture, 0, 0, 512, 301 );
-
-        // here we create the splash image actor and set its size
-        Image splashImage = new Image( splashRegion);
-        splashImage.setWidth(width);
-        splashImage.setHeight(height);
+		// upper-left corner and has a dimension of 512x301
+		TextureRegion splashRegion = new TextureRegion( _mSplashTexture, 0, 0, 512, 301 );
+		
+		// here we create the splash image actor and set its size
+		Image splashImage = new Image( splashRegion);
+		splashImage.setWidth(width);
+		splashImage.setHeight(height);
+		
+		// this is needed for the fade-in effect to work correctly; we're just
+		// making the image completely transparent
+		splashImage.getColor().a = 0f;
         
-        // this is needed for the fade-in effect to work correctly; we're just
-        // making the image completely transparent
-        splashImage.getColor().a = 0f;
-        
-        // configure the fade-in/out effect on the splash image
-        //Fade-in in 0.75 seconds
-        //Wait for 1.75 seconds
-        //Fade-out in 0.75 seconds
-        splashImage.addAction( sequence( fadeIn( 0.75f ), delay( 1.75f ), fadeOut( 0.75f ),
-            new Action() {
-                @Override
-                public boolean act(
-                    float delta )
-                {
-                    // the last action will move to the next screen
-                    _mGame.setScreen( new MenuScreen( _mGame ) );
-                    return true;
-                }
-            } ) );
+		// configure the fade-in/out effect on the splash image
+		//Fade-in in 0.75 seconds
+		//Wait for 1.75 seconds
+		//Fade-out in 0.75 seconds
+		splashImage.addAction( 
+			sequence( 
+				fadeIn( 0.75f ), 
+				delay( 1.75f ), 
+				fadeOut( 0.75f ),
+				new Action() {
+					@Override
+					public boolean act(float delta ){
+						// the last action will move to the next screen
+						_mGame.setScreen( new MenuScreen( _mGame ) );
+						return true;
+					}
+				}
+			)
+            	);
 
-        // and finally we add the actor to the stage
-        _mStage.addActor( splashImage );
+		// and finally we add the actor to the stage
+		_mStage.addActor( splashImage );
 
-        // and finally we add the actors to the stage, on the correct order
-        _mStage.addActor( splashImage );
+		// and finally we add the actors to the stage, on the correct order
+		_mStage.addActor( splashImage );
 	}
 
 	@Override
 	public void show() {
-		// TODO Auto-generated method stub
 		super.show();
 		
 		// load the splash image and create the texture region
-        _mSplashTexture = new Texture( "data/splashscreen.png" );
-        
-        // we set the linear texture filter to improve the stretching
-        _mSplashTexture.setFilter( TextureFilter.Linear, TextureFilter.Linear );
- 
-        // in the image atlas, our splash image begins at (0,0) at the
-        // upper-left corner and has a dimension of 512x301
-        //_mSplashTextureRegion = new TextureRegion( _mSplashTexture, 0, 0, 512, 301 );
+		_mSplashTexture = new Texture( "data/splashscreen.png" );
+		// we set the linear texture filter to improve the stretching
+		_mSplashTexture.setFilter( TextureFilter.Linear, TextureFilter.Linear );
 	}
 
 	@Override
 	public void dispose() {
-		// TODO Auto-generated method stub
 		super.dispose();
 		_mSplashTexture.dispose();
 	}
-
-	
-
-
 }
